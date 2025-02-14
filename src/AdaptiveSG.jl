8caac06df9bade5e4ca82f446818ef73d2b85253
Clpr <zhao1tian1hao1@outlook.com>
2025-02-14 13:21:46 +0800

    add linear extrapolation

--- src/AdaptiveSG.jl
+++ src/AdaptiveSG.jl
@@ -26,6 +26,7 @@ module AdaptiveSG
 
     include("asg/train.jl")    # ASG (first-time) training
     include("asg/update.jl")   # ASG training after grid modification
+    include("asg/extrapolate.jl") # ASG (linear) extrapolation
     include("asg/evaluate.jl") # Interpolant evaluation
     include("asg/integrate.jl") # Numerical integration of ASG/RSG interpolants
 
