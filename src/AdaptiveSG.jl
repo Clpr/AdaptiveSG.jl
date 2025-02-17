7f2ca8cd7069f4e5b0fa14f87884d3013ffc148b
Clpr <zhao1tian1hao1@outlook.com>
2025-02-18 02:36:42 +0800

    add gradient

--- src/AdaptiveSG.jl
+++ src/AdaptiveSG.jl
@@ -28,6 +28,7 @@ module AdaptiveSG
     include("asg/update.jl")   # ASG training after grid modification
     include("asg/extrapolate.jl") # ASG (linear) extrapolation
     include("asg/evaluate.jl") # Interpolant evaluation
+    include("asg/diff.jl")     # Gradient approximation
     include("asg/integrate.jl") # Numerical integration of ASG/RSG interpolants
 
 end # AdaptiveSG
